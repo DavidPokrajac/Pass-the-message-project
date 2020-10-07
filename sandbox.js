67ad8ac81f5acb9a1a8ee5879d5cc427efa0844d
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-07 21:49:44 +0200

    Created logic for when the submit button is clicked; if/else statements for input value provided

--- sandbox.js
+++ sandbox.js
@@ -3,17 +3,36 @@ window.onload = function(){
   // Referencing tags
   var div = document.querySelector("div");
 
-  var form = document.querySelector("form");
+  var form = document.forms[0];
 
-  var input = form.querySelector("input[type='text']");
+  var input = form.querySelector("input");
 
   var message = document.querySelector("#message");
 
-
+  // Adding event listener
   form.addEventListener("submit", function(e){
 
     e.preventDefault();
 
+    var value = input.value;
+
+    // Creating warning paragraph if there is no input
+    var warning = document.createElement("p");
+
+    // If there's no value, display warning message, and make it disappear after few seconds
+    if(value.length === 0){
+      warning.textContent = "You have to input something";
+      div.insertBefore(warning, message);
+      setTimeout(function(){
+        warning.innerHTML = "";
+      }, 3000);
+    } else{
+      message.textContent = value;
+      warning.innerHTML = "";
+    }
+
+    // Resetting the form after each time submit button is clicked
+    form.reset();
   });
 
 }
